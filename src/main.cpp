3656cc7938985d774391e730caad84378555db90
ripkie <rifki.dupon07@gmail.com>
2026-03-02 06:04:03 +0700

    finally konek blynk + firebase

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,21 @@
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
 
+#include <Firebase_ESP_Client.h>
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
+
+// ===== WiFi =====
 char ssid[] = "iPhone";
 char pass[] = "12341234";
 
+// ===== Firebase Config =====
+#define FIREBASE_API_KEY "AIzaSyAhtWZAOYF3f_r4TOQNdIE5hnBgjKwDCvk"
+#define FIREBASE_DATABASE_URL "https://kel1-caascps-acea3-default-rtdb.firebaseio.com"
+#define FIREBASE_USER_EMAIL "rifki.dupon07@gmail.com"
+#define FIREBASE_USER_PASSWORD "Rifkiwidya6*"
+
+// init pin
 #define I2C_SDA 8
 #define I2C_SCL 9
 #define TRIG_PIN 41
@@ -23,9 +35,16 @@ char pass[] = "12341234";
 #define B_PIN 15
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 bool ACTIVE_LOW_SHOCK = false;
 BlynkTimer timer;
 
+// ===== Firebase objects =====
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+// pengaturan lcd
 void lcdPrintLine(byte row, const String &msg)
 {
   lcd.setCursor(0, row);
@@ -56,15 +75,17 @@ void updateSystem()
   int shockRaw = digitalRead(SHOCK_PIN);
   bool shockDetected = ACTIVE_LOW_SHOCK ? (shockRaw == LOW) : (shockRaw == HIGH);
 
-  // LED merah saat shock
+  // LED Nyala saat SHOCk
   digitalWrite(R_PIN, shockDetected ? HIGH : LOW);
   digitalWrite(G_PIN, LOW);
   digitalWrite(B_PIN, LOW);
 
-  // status lcd sir
+  // Status koneksi
   bool wifiOK = (WiFi.status() == WL_CONNECTED);
   bool blynkOK = Blynk.connected();
+  bool firebaseOK = Firebase.ready();
 
+  // LCD
   String line0 = "Jarak:";
   if (distance < 0)
     line0 += "Err";
@@ -73,34 +94,59 @@ void updateSystem()
     line0 += String(distance, 1);
     line0 += " cm";
   }
-  /*line0 += " ";
-  line0 += wifiOK ? "W" : "w";
-  line0 += blynkOK ? "B" : "b"; */
 
   String line1 = "Status: ";
   line1 += shockDetected ? "GEMPAA!!!" : "Normal";
-  // line1 += " V0:";
-  // line1 += shockDetected ? "1" : "0";
 
   lcdPrintLine(0, line0);
   lcdPrintLine(1, line1);
 
-  // Kirim ke blynk
+  // ===== Kirim ke Blynk =====
   if (blynkOK)
   {
-    Blynk.virtualWrite(V0, shockDetected ? 1 : 0); // V0 indikator gempa sir
-    Blynk.virtualWrite(V1, distance);              // V1 jarak
-
-    Serial.print("[SEND OK] Shock=");
-    Serial.print(shockDetected ? 1 : 0);
-    Serial.print(" Jarak=");
-    Serial.print(distance);
-    Serial.println("cm");
+    Blynk.virtualWrite(V0, shockDetected ? 1 : 0);
+    Blynk.virtualWrite(V1, distance);
+    Serial.print("[BLYNK SEND OK] ");
   }
   else
   {
-    Serial.println("[SEND SKIP] Blynk not connected");
+    Serial.print("[BLYNK SKIP] ");
   }
+
+  // send data to firebase
+  if (firebaseOK)
+  {
+    bool ok1 = Firebase.RTDB.setInt(&fbdo, "/devices/esp32s3_01/shock", shockDetected ? 1 : 0);
+    bool ok2 = Firebase.RTDB.setFloat(&fbdo, "/devices/esp32s3_01/distance_cm", distance);
+    bool ok3 = Firebase.RTDB.setInt(&fbdo, "/devices/esp32s3_01/ts_ms", (int)millis());
+
+    if (ok1 && ok2 && ok3)
+    {
+      Serial.print("[FIREBASE SEND OK] ");
+    }
+    else
+    {
+      Serial.print("[FIREBASE FAIL] ");
+      Serial.print(fbdo.errorReason());
+      Serial.print(" ");
+    }
+  }
+  else
+  {
+    Serial.print("[FIREBASE NOT READY] ");
+  }
+
+  // Serial Monitor
+  Serial.print("Shock=");
+  Serial.print(shockDetected ? 1 : 0);
+  Serial.print(" Jarak=");
+  Serial.print(distance);
+  Serial.print("cm WiFi=");
+  Serial.print(wifiOK ? "OK" : "NO");
+  Serial.print(" Blynk=");
+  Serial.print(blynkOK ? "OK" : "NO");
+  Serial.print(" FB=");
+  Serial.println(firebaseOK ? "OK" : "NO");
 }
 
 void setup()
@@ -113,7 +159,6 @@ void setup()
   pinMode(R_PIN, OUTPUT);
   pinMode(G_PIN, OUTPUT);
   pinMode(B_PIN, OUTPUT);
-
   digitalWrite(TRIG_PIN, LOW);
 
   Wire.begin(I2C_SDA, I2C_SCL);
@@ -130,6 +175,7 @@ void setup()
   lcdPrintLine(0, "WiFi");
   lcdPrintLine(1, "Connecting...");
   Serial.print("[WiFi] Connecting");
+
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, pass);
 
@@ -162,7 +208,6 @@ void setup()
   lcdPrintLine(0, "Blynk");
   lcdPrintLine(1, "Connecting...");
   Serial.println("[Blynk] Connecting...");
-
   Blynk.config(BLYNK_AUTH_TOKEN, "blynk.cloud", 80);
   Blynk.connect(5000);
 
@@ -170,7 +215,7 @@ void setup()
   {
     Serial.println("[Blynk] OK Connected!");
     lcdPrintLine(0, "Blynk OK!");
-    lcdPrintLine(1, "Monitoring...");
+    lcdPrintLine(1, "Firebase next..");
   }
   else
   {
@@ -179,8 +224,49 @@ void setup()
     lcdPrintLine(1, "Cek token");
   }
   delay(1000);
+
+  // Firebase init
+  lcdPrintLine(0, "Firebase");
+  lcdPrintLine(1, "Connecting...");
+  Serial.println("[Firebase] Connecting...");
+
+  config.api_key = FIREBASE_API_KEY;
+  config.database_url = FIREBASE_DATABASE_URL;
+
+  auth.user.email = FIREBASE_USER_EMAIL;
+  auth.user.password = FIREBASE_USER_PASSWORD;
+
+  config.token_status_callback = tokenStatusCallback; // dari addons/TokenHelper.h
+
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+
+  // tunggu sebentar biar token kebentuk
+  unsigned long t0 = millis();
+  while (!Firebase.ready() && millis() - t0 < 5000)
+  {
+    delay(200);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  if (Firebase.ready())
+  {
+    Serial.println("[Firebase] OK Ready!");
+    lcdPrintLine(0, "Firebase OK!");
+    lcdPrintLine(1, "Monitoring...");
+  }
+  else
+  {
+    Serial.println("[Firebase] NOT READY!");
+    lcdPrintLine(0, "Firebase FAIL");
+    lcdPrintLine(1, "Cek Auth/RTDB");
+  }
+
+  delay(1000);
   lcd.clear();
 
+  // interval 0.8 detik: baca + kirim
   timer.setInterval(800L, updateSystem);
 }
 
